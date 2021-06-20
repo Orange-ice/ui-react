7e09b93acb4a518ac7cb8c7a6c74c1ced4dd463d
Burt <2902978956@qq.com>
2021-06-20 20:18:43 +0800

    添加 circle ci 配置

---
+++ jest.config.ci.js
@@ -0,0 +1,10 @@
+// https://jestjs.io/docs/en/configuration.html
+const base = require('./jest.config')
+
+module.exports = Object.assign({}, base, {
+  reporters: ["jest-junit"],
+  collectCoverage: true,
+  collectCoverageFrom: ["lib/**/*.{ts,tsx}", "!**/node_modules/**"],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov'],
+})
