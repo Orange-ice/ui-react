8a72ceef49690f4644846c680163213a1a2e38bc
Burt <2902978956@qq.com>
2021-06-23 20:11:41 +0800

    npm publish

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,3 +1,9 @@
 export {default as Icon} from './icon/icon';
+export {default as Dialog} from './dialog/dialog'
+export {default as Layout} from './layout/layout'
+export {default as Aside} from './layout/aside'
+export {default as Header} from './layout/header'
+export {default as Content} from './layout/content'
+export {default as Footer} from './layout/footer'
 
 import './index.scss'
