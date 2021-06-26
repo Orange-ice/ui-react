e332938429d8182d4a356dfc6574239233f4f822
Burt <2902978956@qq.com>
2021-06-26 08:00:58 +0800

    0.0.7

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,15 +1,15 @@
-export {default as Icon} from './icon/icon';
-export {default as Dialog} from './dialog/dialog'
-export {default as Layout} from './layout/layout'
-export {default as Aside} from './layout/aside'
-export {default as Header} from './layout/header'
-export {default as Content} from './layout/content'
-export {default as Footer} from './layout/footer'
+// export {default as Icon} from './icon/icon';
+// export {default as Dialog} from './dialog/dialog'
+// export {default as Layout} from './layout/layout'
+// export {default as Aside} from './layout/aside'
+// export {default as Header} from './layout/header'
+// export {default as Content} from './layout/content'
+// export {default as Footer} from './layout/footer'
 
 import './index.scss'
 
-// import Icon from './icon/icon';
-// import Dialog from './dialog/dialog';
-// import Button from './button/button';
-//
-// export {Icon, Button, Dialog};
+import Icon from './icon/icon';
+import Dialog from './dialog/dialog';
+import Button from './button/button';
+
+export {Icon, Button, Dialog};
