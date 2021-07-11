8ea0f5c1edb47705c400c6c79999a768921be310
Burt <2902978956@qq.com>
2021-07-11 14:55:28 +0800

    Form 表单基本功能

--- example/form-example/FormExample.tsx
+++ example/form-example/FormExample.tsx
@@ -1,11 +1,32 @@
-import React from 'react';
-import Form from '../../lib/form/form';
+import React, {useState} from 'react';
+import Form, {FormValue} from '../../lib/form/form';
 import ExampleLayout from '../ExampleLayout';
+import {Button} from '../../lib';
 
 const FormExample = () => {
+  const [formData, setFormData] = useState<FormValue>({
+    username: 'burt',
+    password: ''
+  });
+  const [fields] = useState([
+    {name: 'username', label: '用户名', input: {type: 'text'}},
+    {name: 'password', label: '密码', input: {type: 'password'}},
+  ]);
+  const onSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
+    console.log(formData);
+  };
   return (
     <ExampleLayout>
-      <Form/>
+      <Form
+        value={formData}
+        fields={fields}
+        onChange={(val) => setFormData(val)}
+        onSubmit={onSubmit}
+        buttons={
+          <>
+            <Button effect="primary">登录</Button>
+          </>
+        }/>
     </ExampleLayout>
   );
 };
--- lib/form/form.tsx
+++ lib/form/form.tsx
@@ -1,8 +1,46 @@
 import React from 'react';
 
-const Form = () => {
+interface FieldsItem {
+  name: string,
+  label: string,
+  input: { type: string }
+}
+
+export interface FormValue {
+  [K: string]: any
+}
+
+interface FormProps {
+  value: FormValue,
+  fields: Array<FieldsItem>,
+  buttons: React.ReactFragment,
+  onChange: (value: FormValue) => void,
+  onSubmit: React.FormEventHandler<HTMLFormElement>
+}
+
+const Form: React.FC<FormProps> = (props) => {
+  const {fields, buttons, value, onChange, onSubmit} = props;
+  const onInputChange = (name: string, val: string) => {
+    const newFormValue = {...value, [name]: val};
+    onChange(newFormValue);
+  };
+  const onFormSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    onSubmit(e);
+  };
   return (
-    <div>form</div>
+    <form onSubmit={onFormSubmit}>
+      {fields.map(field =>
+        <div key={field.name}>
+          {field.label}
+          <input
+            type={field.input.type}
+            value={value[field.name]}
+            onChange={(e) => onInputChange(field.name, e.target.value)}/>
+        </div>
+      )}
+      <div>{buttons}</div>
+    </form>
   );
 };
 
