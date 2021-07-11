8eaee72f9b6912f6416eec8e3687e2dcec924e51
Burt <2902978956@qq.com>
2021-07-11 17:16:28 +0800

    Button example 样式优化

--- example/button-example/ButtonExample.tsx
+++ example/button-example/ButtonExample.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import { Button } from '../../lib';
 import ExampleLayout from '../ExampleLayout';
+import './buttonExample.scss'
 
 const ButtonExample: React.FC = () => {
   return (
     <ExampleLayout>
-      <div>
-        <Button>Default</Button>
-        <Button effect="primary">Primary</Button>
-        <Button variant="ghost" effect="primary">Ghost</Button>
-        <Button variant="outline" effect="primary">Outline</Button>
-        <Button effect="primary" disabled>Disable</Button>
+      <div className="wrapper">
+        <h2>Button 按钮</h2>
+        <p>使用方法</p>
+
+        <br/>
+        <p>基本用法：</p>
+        <div className="buttons">
+          <Button>Default</Button>
+          <Button effect="primary">Primary</Button>
+          <Button variant="outline" effect="primary">Outline</Button>
+          <Button effect="primary" disabled>Disable</Button>
+        </div>
       </div>
     </ExampleLayout>
   );
